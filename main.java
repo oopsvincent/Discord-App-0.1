c36dcbbe1fe90051639e8d492ff6e32b05493a84
Farhan Ali Reza <farhanalireza50@gmail.com>
2024-11-18 23:33:26 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Create a string object
+        String message = "Hello, World!";
+
+        // Print the message to the console
+        System.out.println(message);
+    }
+}
