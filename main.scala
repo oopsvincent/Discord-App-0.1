d047de7b80ce0155758b40a0f1390ac5ba86180c
Farhan Ali Reza <farhanalireza50@gmail.com>
2024-11-18 23:35:22 +0530

    Create main.scala

---
+++ main.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        // Create a string literal
+        val message = "Hello, World!"
+
+        // Print the message to the console
+        println(message)
+    }
+}
