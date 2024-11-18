e75a49f31f676d30f27b93816ec3efab3a6212d3
Farhan Ali Reza <farhanalireza50@gmail.com>
2024-11-18 23:34:13 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Declare a variable to hold the message
+    var greeting string = "Hello, World!"
+
+    // Print the message to the console
+    fmt.Println(greeting)
+}
